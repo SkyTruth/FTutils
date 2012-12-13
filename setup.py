e7be12d0ffc0b6e9f69d0c08711d46adddb7753e
pwoods25443 <paul@skytruth.org>
2012-12-13 15:22:42 -0500

    Initial version

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(name='FTTools',
+      version='1.1.5',
+      
+      description='SkyTruth Fusion Table Tools',
+      author='Paul Woods',
+      author_email='pau@skytruth.org',
+      url='http://www.skytruth.org/',
+      packages=find_packages()
+      )
+      
+#setup(name='FTTools',
+#      version='1.1.5',
+#      
+#      description='SkyTruth Fusion Table Tools',
+#      author='Paul Woods',
+#      author_email='pau@skytruth.org',
+#      url='http://www.skytruth.org/',
+#      packages=find_packages(),
+#      scripts=['scripts/ft-sync']      
+#      )      
