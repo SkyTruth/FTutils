8eacdcbb98648848b38deb6c2e83ebe579752f86
Craig Winters <craigwin.ni@gmail.com>
2013-01-22 08:44:20 -0600

    Extract settings to separate file.  Commit script and settings-template.

---
+++ scripts/settings-template.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 22 08:30:32 2013
+
+@author: Craig
+"""
+
+# standard modules
+
+# site modules
+
+# local modules
+
+# CONSTANTS
+
+FT_USERNAME = "data@skytruth.org"
+FT_PASSWORD = 'password'                # CHANGE
+DB_HOST     = 'localhost'
+DB_USER     = 'scraper'
+DB_PASSWORD = 'password'                # CHANGE
