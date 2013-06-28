b08a91317dd94ad23a2567d292423bdc36fc01b9
PaulWoods <paul@skytruth.org>
2013-06-28 17:22:48 +0000

    update version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='FTTools',
-      version='1.1.5',
+      version='1.1.6',
       
       description='SkyTruth Fusion Table Tools',
       author='Paul Woods',
