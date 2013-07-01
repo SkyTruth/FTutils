e9fb39cf532667770a5d941e020d38f3ac5fe6fe
Craig <craigwin.ni@gmail.com>
2013-07-01 15:27:07 +0000

    Finish cleanup of FTutils libraries.

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ FTutils
 =======
 
 Wrapper for Google Fusion Tables API
+
+To install:
+	Install the packages in the ./site-packages directory:
+		easy_install ./site-packages/*.zip
+	Install this package:
+		python setup.py sdist
+		easy_install dist/FTTools-1.1.6.tar.gz
+
--- scripts/testFTSync.s
+++ scripts/testFTSync.s
@@ -1,10 +1,13 @@
-#!bash
+#bash
 
 # THIS SHOULD NOT BE RUN ON THE SERVER. 
 # IT WILL CORRUPT THE SYNC BETWEEN THE DATABASE AN THE TABLE.
 # EXECUTE ON A MACHINE WITH A SCRATCH DATABASE.
+if [ `uname -n` = 'ewn3' ] ; then echo THIS TEST DOES NOT OPERATE ON ewn3; exit; fi
 
-#/srv/scraper/bin/python /srv/scraper/FTutils/scripts/ft-sync DB2FT --seq_field=st_id --max_recs=1000 @PA_Spud PA_Spud
+# for ewn4
+/srv/scraper/bin/python ./ftapi-sync.py DB2FT --seq_field=st_id --max_recs=150 @TestFTSync_CogisSpill CogisSpill
 
-~/Dev/SkyTruth1/Scripts/python.exe ./ftapi-sync.py DB2FT --seq_field=st_id --max_recs=150 @TestFTSync_CogisSpill CogisSpill
+# for windows machines
+#~/Dev/SkyTruth1/Scripts/python.exe ./ftapi-sync.py DB2FT --seq_field=st_id --max_recs=150 @TestFTSync_CogisSpill CogisSpill
 
