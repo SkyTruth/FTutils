ff432782f1594f2d37d102c694eab7b7f8db1385
PaulWoods <paul@skytruth.org>
2013-10-24 08:58:01 +0000

    Committing changes found on prod server... Don't leave changes uncommitted on prodserver, please ;)

---
+++ scripts/PA_rescrape-DB2FT.s
@@ -0,0 +1,5 @@
+
+#/srv/scraper/bin/python /srv/scraper/FTutils/scripts/ftapi-sync.py DB2FT --seq_field=st_id --max_recs=40000 @PA_Spud PA_Spud
+/srv/scraper/bin/python /srv/scraper/FTutils/scripts/ftapi-sync.py DB2FT --seq_field=st_id --max_recs=60000 @PA_DrillingPermit PA_DrillingPermit
+#/srv/scraper/bin/python /srv/scraper/FTutils/scripts/ftapi-sync.py DB2FT --seq_field=st_id --max_recs=30000 @PA_Violation PA_Violation
+
