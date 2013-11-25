56b6117ba84035070674c1d80c7a8fc09a3eb7b0
User scrapy on scrape.skytruth.org <scrapy@skytruth.org>
2013-11-25 21:24:21 +0000

    Reactivate FTutils on scraper.skytruth.org.

--- scripts/skytruth-DB2FT.s
+++ scripts/skytruth-DB2FT.s
@@ -1,12 +1,11 @@
 
-/srv/scraper/bin/python /srv/scraper/FTutils/scripts/ft-sync DB2FT --seq_field=st_id --max_recs=1000 @PA_Spud PA_Spud
-/srv/scraper/bin/python /srv/scraper/FTutils/scripts/ft-sync DB2FT --seq_field=st_id --max_recs=1000 @PA_DrillingPermit PA_DrillingPermit
-/srv/scraper/bin/python /srv/scraper/FTutils/scripts/ft-sync DB2FT --seq_field=st_id --max_recs=1000 @PA_Violation PA_Violation
-/srv/scraper/bin/python /srv/scraper/FTutils/scripts/ft-sync DB2FT --seq_field=seqid --max_recs=1000 @FracFocusReport FracFocusReport
-/srv/scraper/bin/python /srv/scraper/FTutils/scripts/ft-sync DB2FT --seq_field=st_id --max_recs=1000 @CogisInspection CogisInspection
-/srv/scraper/bin/python /srv/scraper/FTutils/scripts/ft-sync DB2FT --seq_field=st_id --max_recs=1000 @CogisSpill CogisSpill
-/srv/scraper/bin/python /srv/scraper/FTutils/scripts/ft-sync DB2FT --seq_field=st_id --max_recs=1000 @WV_DrillingPermit WV_DrillingPermit
-/srv/scraper/bin/python /srv/scraper/FTutils/scripts/ft-sync DB2FT --seq_field=reportnum --max_recs=5000 @NrcScrapedReport NrcScrapedReport
-/srv/scraper/bin/python /srv/scraper/FTutils/scripts/ft-sync DB2FT --seq_field=reportnum --max_recs=5000 @NrcParsedReport NrcParsedReport
-/srv/scraper/bin/python /srv/scraper/FTutils/scripts/ft-sync DB2FT --seq_field=st_id --max_recs=5000 @NrcScrapedMaterial NrcScrapedMaterial
-
+/srv/scrapy/bin/python /srv/scrapy/FTutils/scripts/ftapi-sync.py DB2FT --seq_field=st_id --max_recs=1000 @PA_Spud PA_Spud
+/srv/scrapy/bin/python /srv/scrapy/FTutils/scripts/ftapi-sync.py DB2FT --seq_field=st_id --max_recs=1000 @PA_DrillingPermit PA_DrillingPermit
+/srv/scrapy/bin/python /srv/scrapy/FTutils/scripts/ftapi-sync.py DB2FT --seq_field=st_id --max_recs=1000 @PA_Violation PA_Violation
+/srv/scrapy/bin/python /srv/scrapy/FTutils/scripts/ftapi-sync.py DB2FT --seq_field=seqid --max_recs=1000 @FracFocusReport FracFocusReport
+/srv/scrapy/bin/python /srv/scrapy/FTutils/scripts/ftapi-sync.py DB2FT --seq_field=st_id --max_recs=1000 @CogisInspection CogisInspection
+/srv/scrapy/bin/python /srv/scrapy/FTutils/scripts/ftapi-sync.py DB2FT --seq_field=st_id --max_recs=1000 @CogisSpill CogisSpill
+/srv/scrapy/bin/python /srv/scrapy/FTutils/scripts/ftapi-sync.py DB2FT --seq_field=st_id --max_recs=1000 @WV_DrillingPermit WV_DrillingPermit
+/srv/scrapy/bin/python /srv/scrapy/FTutils/scripts/ftapi-sync.py DB2FT --seq_field=reportnum --max_recs=5000 @NrcScrapedReport NrcScrapedReport
+/srv/scrapy/bin/python /srv/scrapy/FTutils/scripts/ftapi-sync.py DB2FT --seq_field=reportnum --max_recs=5000 @NrcParsedReport NrcParsedReport
+/srv/scrapy/bin/python /srv/scrapy/FTutils/scripts/ftapi-sync.py DB2FT --seq_field=st_id --max_recs=5000 @NrcScrapedMaterial NrcScrapedMaterial
--- scripts/testFTSync.s
+++ scripts/testFTSync.s
@@ -6,7 +6,8 @@
 if [ `uname -n` = 'ewn3' ] ; then echo THIS TEST DOES NOT OPERATE ON ewn3; exit; fi
 
 # for ewn4
-/srv/scraper/bin/python ./ftapi-sync.py DB2FT --seq_field=st_id --max_recs=150 @TestFTSync_CogisSpill CogisSpill
+#/srv/scraper/bin/python ./ftapi-sync.py DB2FT --seq_field=st_id --max_recs=150 @TestFTSync_CogisSpill CogisSpill
+/srv/scrapy/bin/python ./ftapi-sync.py DB2FT --seq_field=st_id --max_recs=150 @TestFTSync_CogisSpill CogisSpill
 
 # for windows machines
 #~/Dev/SkyTruth1/Scripts/python.exe ./ftapi-sync.py DB2FT --seq_field=st_id --max_recs=150 @TestFTSync_CogisSpill CogisSpill
